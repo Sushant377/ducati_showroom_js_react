2497bfe77a6e7b745ef69ef01f176dfed62c2adb
Sushan Lamichhane <85974988+Sushant377@users.noreply.github.com>
2024-02-05 16:58:59 +0545

    card updated on test ride

--- src/content/Testride.jsx
+++ src/content/Testride.jsx
@@ -1,8 +1,36 @@
+import Wed from "../practise/wed-card/wed-card";
+import "../practise/wed-card/Wed-card.css";
+
 function Testride() {
+  const title = "श्री गणेशाए नमह:";
+  const wedimage1 = "./ganesh.png";
+  const wedimage2 = "ramsita.jpg";
+  const nimantrana = "हार्दिक निमन्त्रणा ";
+  const description = {
+    behula: "राम",
+    sanga: "संग",
+    behuli: "सीता",
+    miti: "  चैत्र १६ २०८१ | सोमबार",
+    desc1: "बैबाहिक कार्यक्रम ",
+    jantijane: "जन्ती जाने ",
+    samaye: "समय: बिहान ९ बजे ",
+    sthan: "स्थान: अयोध्या",
+    pritibhoj: "प्रितिभोज",
+    bhojsamaye: "समय: बिहान ११ बजे ",
+    bhojmiti: "मिति: चैत्र १७  २०८१ ",
+    ayodhyapariwar: "अयोध्या परिवार ",
+  };
   return (
     <div>
-      <h1>Sorry !</h1>
-      <h1>All bikes are reserved currently.</h1>
+      <Wed
+        title={title}
+        wedimage1={wedimage1}
+        wedimage2={wedimage2}
+        nimantrana={nimantrana}
+        description={description}
+      >
+        <h3> {nimantrana}</h3>
+      </Wed>
     </div>
   );
 }
--- src/practise/wed-card/Wed-card.jsx
+++ src/practise/wed-card/Wed-card.jsx
@@ -1,6 +1,44 @@
+/* eslint-disable react/prop-types */
+import "./Wed-card.css";
 function Wed(props) {
-  const { title, children, wedimage } = props;
-  return <div>{children}</div>;
+  const { title, children, wedimage1, wedimage2, description } = props;
+  console.log(description);
+  return (
+    <div className="wed-card">
+      <div className="title-card">
+        <h4>{title}</h4>
+      </div>
+      <div className="wed-images">
+        <div> {<img className="wed-image1" src={wedimage1} />}</div>
+        <div> {<img className="wed-image2" src={wedimage2} />}</div>
+      </div>
+      {children && <div className="children">{children}</div>}
+      <div className="behulasangabehuli">
+        <h1>{description.behula}</h1>
+        <h3 style={{ paddingTop: "10px" }}>{description.sanga}</h3>
+        <h1>{description.behuli}</h1>
+      </div>
+      <div className="miti_bar">
+        <h4>{description.miti}</h4>
+        <h4>{description.desc1}</h4>
+      </div>
+      <div className="baibahik_karyekram">
+        <div>
+          <h5>{description.jantijane}</h5>
+          <h6>{description.sthan}</h6>
+          <h6>{description.samaye}</h6>
+        </div>
+        <div>
+          <h5>{description.pritibhoj}</h5>
+          <h6>{description.bhojsamaye}</h6>
+          <h6>{description.bhojmiti}</h6>
+        </div>
+      </div>
+      <div className="ayodhyapariwar">
+        <h2>{description.ayodhyapariwar}</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Wed;
